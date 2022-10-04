6f95fc51ce435227b3c7ac7a37edb6cae50e0677
Harvey MAO <harvey.mao@outlook.com>
2022-10-05 03:06:28 +0800

    update

--- 08_shortest_subarray_with_sum_at_least_k/demo.cc
+++ 08_shortest_subarray_with_sum_at_least_k/demo.cc
@@ -1,5 +1,7 @@
 #include <vector>
 #include <iostream>
+#include <deque>
+
 using namespace std;
 
 /*
@@ -64,6 +66,79 @@ public:
 
         return INT_MAX == min_len ? 0 : min_len;
     }
+
+    /*
+       For double pointers, it is better to fix one and change another alternatively.
+    */
+    int minSubArrayLen_better(int target, vector<int>& nums)
+    {
+        int sum = 0;
+        int l = 0;
+        int min_len = INT_MAX;
+        for (int r = 0; r < int(nums.size()); ++r)
+        {
+            sum += nums[r];
+
+            /* We need check sum > target*/
+            /* No need to consider the  0 to last l_min since it is worse/longer!*/
+            while (l <= r && sum >= target)
+            {
+                min_len = min<int>(min_len, r - l + 1);
+                sum-=nums[l];
+                ++l;
+            }
+        }
+
+        return min_len == INT_MAX ? 0 : min_len;
+    }
+    /*
+       Here, the integer is allowed to be negative, leading to failure of double pointers!
+       https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+    */
+    int shortestSubarray(vector<int>& nums, int k)
+    {
+        /*p[i] = nums[0] + nums[1] + ... + nums[i-1] for i = 0, 1, 2, len(nums)
+          p[i + 1] = p[i] + nums[i] for i = 1, 2, 3, ..., len(nums)
+          p[0] = 0, p[1] = nums[0]
+          Here, we use semi-open interval nums[x, y).
+          sum of nums[x, y) = p[y] - p[x] length = y - x
+        */
+        std::vector<long int> p(nums.size() + 1, 0);
+        for (int i = 1; i <= nums.size(); ++i)
+        {
+            p[i] = p[i - 1] + nums[i - 1];
+        }
+
+        /* Maintain a "monoqueue" of indices of P: a deque of indices x_0, x_1, ... such that
+           P[x_0], P[x_1], ... is increasing.*/
+        deque<int> mono_queue;
+        int min_len = INT_MAX;
+        /* Fix r and check all l <= r */
+        /* Now we work with p*/
+        for (int r = 0; r < p.size(); ++r)
+        {
+            /* pop_back all elements which are >= nums[r] before push back */
+            while (!mono_queue.empty() && p[r] <= p[mono_queue.back()])
+            {
+                mono_queue.pop_back();
+            }
+
+
+            /* Since we work with [l, r), we need pop_front before we insert!
+               avoid [r, r) wrong!  zero length
+            */
+            while ((!mono_queue.empty()) && (p[r] - p[mono_queue.front()] >= k))
+            {
+                min_len = min<int>(min_len, r - mono_queue.front());
+                mono_queue.pop_front();
+            }
+
+            mono_queue.push_back(r);
+
+        }
+
+        return min_len == INT_MAX ? -1 : min_len;
+    }
 };
 
 int main()
@@ -77,9 +152,11 @@ int main()
 
     /* int target = 11; */
     /* vector<int> nums = {1,1,1,1,1,1,1,1}; */
-    int target = 7;
-    vector<int> nums = {1,1,1,1,7};
-    int min_len = sol.minSubArrayLen(target, nums);
+    /* int target = 7; */
+    /* vector<int> nums = {1,1,1,1,7}; */
+    vector<int> nums = {2,-1,2};
+    int k = 3;
+    int min_len = sol.shortestSubarray(nums, k);
 
     cout << "min_len " << min_len << endl;
 
