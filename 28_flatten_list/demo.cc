120c085d54a6cde415271dd7c5ac5de70d8325e9
Harvey MAO <harvey.mao@outlook.com>
2022-10-28 00:15:17 +0800

    tree traversal

---
+++ 28_flatten_list/demo.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+
+        /* Might need to rectify. */
+        return nullptr;
+    }
+};
+
+int main()
+{
+    return 0;
+}
