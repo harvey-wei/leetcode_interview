fd2c2a708e22e1e8df0022bf02032c5c7b943b30
Harvey MAO <harvey.mao@outlook.com>
2022-10-31 22:16:28 +0800

    Flatten a multi-level doubly linked list

--- 28_flatten_list/demo.cc
+++ 28_flatten_list/demo.cc
@@ -15,10 +15,62 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
+        stack<Node*> stack_nodes;
+        Node* curr = head;
+
+        while (nullptr != curr)
+        {
+            if (nullptr != curr->child && nullptr != curr->next)
+            {
+                stack_nodes.push(std::move(curr->next));
+                curr->next = curr->child;
+                curr->child = nullptr;
+                curr->next->prev = curr;
+                // update curr
+                curr = curr->next;
+            }
+            else if (nullptr != curr->next)
+            {
+                // update curr
+                curr = curr->next;
+            }
+            else if (nullptr != curr->child)
+            {
+                curr->next = curr->child;
+                curr->child = nullptr;
+                curr->next->prev = curr;
+
+                // update curr
+                curr = curr->next;
+            }
+            else
+            {
+                /* both child and next are nullptr. */
+                if (!stack_nodes.empty())
+                {
+                    Node * top = stack_nodes.top();
+                    stack_nodes.pop();
+                    curr->next = top;
+                    top->prev = curr;
+
+                    // update curr
+                    curr = top;
+
+                }
+                else
+                {
+                    curr = nullptr;
+                }
+            }
+        }
 
         /* Might need to rectify. */
-        return nullptr;
+        return head;
     }
+
+    /* TODO: The problem can be reduced to pre-order tree traversal.
+       Hence, morris traversal is the space-efficient choice.
+    */
 };
 
 int main()
