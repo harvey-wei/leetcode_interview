80c31a5d1c2451146bcea1517d4fb407aa76e91c
Harvey MAO <harvey.mao@outlook.com>
2022-11-04 02:18:40 +0800

    Insert into circular sorted list

--- 29_insert_into_a_sorted_circular_list/demo.cc
+++ 29_insert_into_a_sorted_circular_list/demo.cc
@@ -22,6 +22,70 @@ public:
 
 class Solution {
 public:
+    Node* insert_efficient(Node* head, int insertVal)
+    {
+        // case 1: empty list
+        if (nullptr == head)
+        {
+            head = new Node(insertVal);
+            head->next = head;
+
+            return head;
+        }
+
+        // case 2: one-node list
+        if (head->next == head)
+        {
+            Node* node_new = new Node(insertVal);
+            head->next = node_new;
+            node_new->next = head;
+
+            return head;
+        }
+
+        // case 3: count of nodes >= 2
+        Node* node_new = new Node(insertVal);
+        Node* curr = head;
+        Node* next = head->next;
+
+        // Advance curr and next if not find the proper position and will not advance to the origin.
+        // Think in advance!
+        // subcase 1: insertVal lies between smallest and biggest.
+        // subcase 2: insertVal is <= smallest or >= biggest. biggest->insertVal->smallest(next of
+        // biggest)
+        Node* biggest = head;
+        while (!(curr->val <= insertVal && insertVal <= next->val)
+                && next != head)
+        {
+            // advance
+            curr = curr->next;
+            next = next->next;
+
+            // >= is required
+            // 1 2 3 3 and insert 4 head = 3 we need the last 3
+            if (curr->val >= biggest->val)
+            {
+                biggest = curr;
+            }
+        }
+
+        if (curr->val <= insertVal && insertVal <= next->val)
+        {
+            curr->next = node_new;
+            node_new->next = next;
+
+        }
+        else
+        {
+            /* subcase 2 */
+            Node* smallest = biggest->next;
+            biggest->next = node_new;
+            node_new->next = smallest;
+        }
+
+        return head;
+    }
+
     Node* insert(Node* head, int insertVal)
     {
         // case 1: empty list.
