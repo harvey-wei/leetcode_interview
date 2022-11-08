68d62d59087de6c9561e6dce6784e15de0c13c0c
Harvey MAO <harvey.mao@outlook.com>
2022-11-09 01:23:49 +0800

    Monotonic Stack

---
+++ 37_asteroid_collison/demo.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        /*
+           + ->
+           - <-
+           + -> <- -
+           if current is - <-, we need decide whether it will collide with top.
+           if current is + <-, push to stack directly.
+           if stack is empty, push to stack
+
+           Note that: vector<int> can simulate stack
+        */
+        vector<int> stack;
+
+        for (int i = 0; i < asteroids.size(); ++i)
+        {
+            if (stack.empty())
+            {
+                stack.push_back(std::move(asteroids[i]));
+            }
+            else
+            {
+                /* not empty */
+                if (asteroids[i] > 0)
+                {
+                    stack.push_back(std::move(asteroids[i]));
+                }
+                else
+                {
+                    /* current is < 0. */
+                    bool is_curr_destroyed = false;
+
+                    while (!is_curr_destroyed && !stack.empty() && stack.back() > 0)
+                    {
+                        if (abs(asteroids[i]) > abs(stack.back()))
+                        {
+                            stack.pop_back();
+                        }
+                        else if (abs(asteroids[i]) == abs(stack.back()))
+                        {
+                            is_curr_destroyed = true;
+                            stack.pop_back();
+                        }
+                        else
+                        {
+                            is_curr_destroyed = true;
+                        }
+                    }
+
+                    if (!is_curr_destroyed)
+                    {
+                        stack.push_back(std::move(asteroids[i]));
+                    }
+                }
+
+            }
+        }
+
+        return stack;
+
+    }
+};
+
+int main()
+{
+    return 0;
+}
---
+++ 38_daily_temp/demo.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        /*
+           Monotonic stacks are a good option when a problem involves comparing the size of
+           numeric elements, with their order being relevant.
+
+           Since we need find the most recent day when the temperature is higher than the current.
+           And need to main the descending order.
+        */
+        vector<int> result(temperatures.size(), 0);
+        stack<int> stack_idx;
+
+        for (int i = 0; i < temperatures.size(); ++i)
+        {
+            if (stack_idx.empty())
+            {
+                stack_idx.push(std::move(i));
+            }
+            else
+            {
+                /* pop until current is smaller than the top. */
+                while (!stack_idx.empty() && temperatures[stack_idx.top()] < temperatures[i])
+                {
+                    /* Pop out the idx for which the number of waiting days is found. */
+                    result[stack_idx.top()] = i - stack_idx.top();
+                    stack_idx.pop();
+                }
+
+                stack_idx.push(i);
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    return 0;
+}
