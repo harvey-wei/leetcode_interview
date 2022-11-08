f6be9c76a02b1feb9e202924b7f271d9ecbd8344
Wei MAO <wei.mao@uisee.com>
2022-11-08 19:59:00 +0800

    reverse polish

--- 36_reverse_polish_notation/demo.cc
+++ 36_reverse_polish_notation/demo.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 class Solution {
 public:
-    int evaluate(const int& lhs, const int& rhs, const string& ops)
+    long long int evaluate(const long long int& lhs, const long long int& rhs, const string& ops)
     {
         if (ops == "+")
         {
@@ -29,19 +29,11 @@ public:
             return lhs / rhs;
         }
     }
-    int evalRPN(vector<string>& tokens)
+    long long int evalRPN(vector<string>& tokens)
     {
         /* Binary operator leads to length of tokens >= 3. */
-        stack<int> operands;
-        int number;
-        int res = 0;
+        stack<long long int> operands;
         bool is_start = false;
-
-        if (1 == tokens.size())
-        {
-            istringstream(tokens[0]) >> number;
-            return number;
-        }
 
         for (uint i = 0; i < tokens.size(); ++i)
         {
@@ -50,31 +42,21 @@ public:
                     && tokens[i] != "*"
                     && tokens[i] != "/")
             {
-                istringstream(tokens[i]) >> number;
-                operands.push(std::move(number));
+                operands.push(std::move(stoll(tokens[i])));
             }
             else
             {
-                if (!is_start)
-                {
-                    int rhs = operands.top();
-                    operands.pop();
-                    int lhs = operands.top();
-                    operands.pop();
-                    res = evaluate(lhs, rhs, tokens[i]);
-                    is_start = true;
-                }
-                else
-                {
-                    int lhs = operands.top();
-                    operands.pop();
-                    res = evaluate(lhs, res, tokens[i]);
-                }
+                long long int num_one = operands.top();
+                operands.pop();
+                long long int num_two = operands.top();
+                operands.pop();
 
+                long long int res = evaluate(num_two, num_one, tokens[i]);
+                operands.push(std::move(res));
             }
         }
 
-        return res;
+        return operands.top();
     }
 };
 
@@ -83,7 +65,8 @@ int main()
     Solution sol;
     /* vector<string> tokens = {"2","1","+","3","*"}; */
     /* vector<string> tokens = {"4","13","5","/","+"}; */
-    vector<string> tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+    /* vector<string> tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"}; */
+    vector<string> tokens= {"-128","-128","*","-128","*","-128","*","8","*","-1","*"};
     std::cout << "res " << sol.evalRPN(tokens) << std::endl;
     return 0;
 }
